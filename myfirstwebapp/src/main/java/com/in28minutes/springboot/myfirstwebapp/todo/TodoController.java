48807c96acffba3504f23d4ecf4952185fe22eaf
youngmoo99 <rladuan612@gmail.com>
2023-12-08 18:44:52 +0900

    8days commit
    12-08 commit

---
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -0,0 +1,44 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name") //세션에 값 저장
+public class TodoController {
+	public TodoController(TodoService todoService) {
+		super();
+		this.todoService = todoService;
+	}
+	
+	private TodoService todoService;
+	
+	
+	//list-todos
+	@RequestMapping("list-todos")
+	public String listAllTodos(ModelMap model) {
+		List<Todo> todos = todoService.findByUsername("in28minutes");
+		model.addAttribute("todos",todos);
+		return "listTodos";
+		
+	}
+	
+	//GET, POST
+	@RequestMapping(value="add-todo" , method = RequestMethod.GET)
+	public String showNewTodoPage() {	
+		return "todo";
+		
+	}
+	
+	@RequestMapping(value="add-todo" , method = RequestMethod.POST)
+	public String addNewTodo() {	
+		return "redirect:list-todos";
+		
+	}
+	
+}
