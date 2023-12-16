6bb71f7ccfaf307775f732c7a5d036e27ce27b05
youngmoo99 <rladuan612@gmail.com>
2023-12-16 20:19:18 +0900

    14days commit
    2023-12-16 commit

---
+++ restful-web-services/src/main/java/com/in28minutes/ret/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,36 @@
+package com.in28minutes.ret.webservices.restfulwebservices.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+	//timestamp 
+	//message
+	//details 
+	private LocalDateTime timestamp; 
+	private String message;
+	private String details;
+	
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetails() {
+		return details;
+	} 
+	
+	
+	
+	
+}
--- restful-web-services/src/main/java/com/in28minutes/ret/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/ret/webservices/restfulwebservices/user/UserResource.java
@@ -6,12 +6,15 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 //순서 
 //localhost로 /users호출하면 GetMapping으로 들어와서 UserDaoService로 간다음 
 // -> 모든 사용자 정보를 반환하고 그걸 UserResource에서 받아 JSON으로 변환해서 결과가 보임 
@@ -40,9 +43,15 @@ public class UserResource {
 		return user;
 	}
 	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		service.deleteById(id);
+		
+	}
+	
 	//POST /users 사용자 생성 
 	@PostMapping("/users")
-	public ResponseEntity<User> createUser(@RequestBody User user) { //메소드 인자를 웹 요청의 본문과 매핑
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user) { //메소드 인자를 웹 요청의 본문과 매핑
 		User savedUser = service.save(user);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().
 				path("/{id}").
